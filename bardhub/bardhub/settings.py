587d0eff912a8a380dd2b2c2f9d9b40266e16161
Viet Trinh <viettrinh.ce@gmail.com>
2020-03-19 21:31:01 -0600

    set superuser and connect to database

--- bardhub/bardhub/settings.py
+++ bardhub/bardhub/settings.py
@@ -80,13 +80,25 @@ WSGI_APPLICATION = 'bardhub.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+"""
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'test_2',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'USER': 'root',
+        'PASSWORD': 'Trinh123',
+    }
+}
+
 
 
 # Password validation
--- bardhub/leads/models.py
+++ bardhub/leads/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 class Lead(models.Model):
     name = models.CharField(max_length =100)
-    email = models.EmailField(max_length= 500, unique=True)
-    message = models.CharField(max_length=500, blank=True)
+    email = models.EmailField(max_length= 255, unique=True)
+    message = models.CharField(max_length=255, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     
