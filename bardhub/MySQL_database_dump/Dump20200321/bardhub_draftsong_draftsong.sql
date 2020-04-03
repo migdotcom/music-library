14f8b2ed800fb829bf61924e7136179b5ae011e5
Viet Trinh <viettrinh.ce@gmail.com>
2020-04-03 01:11:00 -0600

    merged stuff

---
+++ bardhub/MySQL_database_dump/Dump20200321/bardhub_draftsong_draftsong.sql
@@ -0,0 +1,56 @@
+-- MySQL dump 10.13  Distrib 8.0.19, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: bardhub
+-- ------------------------------------------------------
+-- Server version	8.0.19
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `draftsong_draftsong`
+--
+
+DROP TABLE IF EXISTS `draftsong_draftsong`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `draftsong_draftsong` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `Image` varchar(255) DEFAULT NULL,
+  `Description` varchar(255) DEFAULT NULL,
+  `Name` varchar(80) DEFAULT NULL,
+  `Time_stamp` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `Album_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `draftsong_draftsong_Album_id_c5ed39f2_fk_album_album_id` (`Album_id`),
+  CONSTRAINT `draftsong_draftsong_Album_id_c5ed39f2_fk_album_album_id` FOREIGN KEY (`Album_id`) REFERENCES `album_album` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `draftsong_draftsong`
+--
+
+LOCK TABLES `draftsong_draftsong` WRITE;
+/*!40000 ALTER TABLE `draftsong_draftsong` DISABLE KEYS */;
+/*!40000 ALTER TABLE `draftsong_draftsong` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-03-21 20:36:14
---
+++ bardhub/draftsong/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class DraftsongConfig(AppConfig):
+    name = 'draftsong'
