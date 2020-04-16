af4d325158acdfd0ef4e91bd822fd338350fa639
cidetraq <nranda99@gmail.com>
2020-04-16 01:09:23 -0500

    changes in progress to making second report

--- bardhub/frontend/src/actions/users.js
+++ bardhub/frontend/src/actions/users.js
@@ -7,8 +7,46 @@ export const getUser = username => dispatch => {
     axios
     .get("/api/users/", username)
 	.then(res => {
-        console.log(res.data);
-        }
+        dispatch({
+            type: GET_USER
+            payload: res2.data
+        });
+                }
     )
     .catch(err => console.log(err));
 };
+/* 
+export const getUserTotalPlaycount = userid => dispatch => {
+    axios
+    .get("/api/users/${userid}")
+	.catch(
+    err => console.log("Error getting user from query "+ userid)
+    )
+    .then(res => {
+        //console.log(res.data);
+        var id_params = res.data[0].id;
+        if (typeof(id_params) == "number"){
+            axios
+            .get("/api/albums/", {
+                params: {
+                    id: id_params
+                }
+            })
+            .then(res2 => {
+                dispatch({
+                    type: GET_TRACKS,
+                    payload: res2.data
+                });
+            })
+            .catch(err => console.log(err));
+        }
+    })
+    .catch(
+    err => console.log("Error getting albums from user id ")
+/*    , dispatch({
+        type: "error",
+        payload: noResults
+    }) */
+    // todo: create noResults object in our code (or maybe in the database under a specific user) that will show upon No Results preventing bugs that we have
+    );
+}; */
--- bardhub/user/api.py
+++ bardhub/user/api.py
@@ -13,3 +13,14 @@ class UserViewSet(viewsets.ModelViewSet):
         return queryset
     permissions_classes = [permissions.AllowAny]
     serializer_class = UserSerializer
+   
+# class UserTotalPlaycount(viewsets.ModelViewSet):
+    # def get_queryset(self):
+        # #need to protect against SQL injection with the quotes in the future
+        # query = "SELECT SUM(Count) FROM album_album WHERE id IN (SELECT id FROM user_user WHERE username=%s", username;
+        # queryset = User.objects.raw("SELECT * FROM album_album WHERE Time_stamp BETWEEN date('now','-1 month') AND date('now','+1 day')");
+        # return queryset
+    # #We're gonna get this info and then on the front end summarize it into a single number, and explain to professor that getting it to work with our framework otherwise would be impossible to finish in our time limit
+    # permissions_classes = [  
+        # permissions.AllowAny ]
+    # serializer_class = AlbumSerializer
