e0f7e2c7381bca9feaf1b5bd534b6f76c25387ae
cidetraq <nranda99@gmail.com>
2020-04-18 10:49:41 -0500

    made changes without migrations to reflect Ryan's migrations changes

--- bardhub/playlist/migrations/0002_playlist_user.py
+++ bardhub/playlist/migrations/0002_playlist_user.py
@@ -2,7 +2,6 @@
 
 from django.conf import settings
 from django.db import migrations, models
-import django.db.models.deletion
 
 
 class Migration(migrations.Migration):
@@ -10,14 +9,14 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
+        ('playlist', '0001_initial'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('artist', '0001_initial'),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='artist',
+            model_name='playlist',
             name='User',
-            field=models.OneToOneField(blank=True, default=None, null=True, on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL),
+            field=models.ManyToManyField(blank=True, to=settings.AUTH_USER_MODEL),
         ),
     ]
